f580b06545fb84a105d9ce1db4e343237c680495
Maedah Batool <maedahbatool@gmail.com>
2018-07-05 17:35:45 +0500

    👌 IMPROVE: Default Task

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,48 @@
  * @version 1.0.0
  */
 
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var sass = require('gulp-sass');
+// Configure.
+const config = {
+	pugSrc: './views/**/*.pug',
+	pugDst: './',
+	scssSrc: './scss/style.scss',
+	scssDst: './',
+	pugWatchFiles: './views/**/*.pug',
+	scssWatchFiles: './scss/**/*.scss'
+};
+
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
 
 gulp.task('pug', function() {
 	return gulp
-		.src('./views/**/*.pug')
+		.src(config.pugSrc)
 		.pipe(
 			pug({
 				pretty: true
 			})
 		)
-		.pipe(gulp.dest('./'));
+		.pipe(gulp.dest(config.pugDst));
 });
 
 gulp.task('scss', function() {
 	return gulp
-		.src('./sass/style.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./'));
+		.src('config.scssSrc')
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(gulp.dest('config.scssDst'));
 });
+
+gulp.task('default', ['pug', 'scss'], function() {
+	gulp.watch(config.pugWatchFiles, ['pug']);
+	gulp.watch(config.scssWatchFiles, ['scss']);
+});
+
+// gulp.task('default', ['pug', 'scss'], function () {
+// 	gulp.watch(pugFiles, 'pug');
+// 	gulp.watch(scssFiles, 'scss');
+// });
 
 // gulp.task('hi', function() {
 // 	console.log('Hi Meow');
-// });
-
-// gulp.task('default', ['pug', 'scss'], function() {
-// 	gulp.watch(pugFiles, 'pug');
-// 	gulp.watch(scssFiles, 'scss');
 // });
