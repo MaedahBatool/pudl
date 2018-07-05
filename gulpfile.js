ef78a77bdc94ba0ed4b694bee566b456aad9385e
Maedah Batool <maedahbatool@gmail.com>
2018-07-05 17:09:21 +0500

    📦 NEW: First draft

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,32 @@
  */
 
 var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
 
-/**
- * Gulp Task.
- *
- */
-gulp.task('hello', function() {
-	console.log('Hello Mao');
+gulp.task('pug', function() {
+	return gulp
+		.src('./views/**/*.pug')
+		.pipe(
+			pug({
+				pretty: true
+			})
+		)
+		.pipe(gulp.dest('./'));
 });
+
+gulp.task('scss', function() {
+	return gulp
+		.src('./sass/style.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./'));
+});
+
+// gulp.task('hi', function() {
+// 	console.log('Hi Meow');
+// });
+
+// gulp.task('default', ['pug', 'scss'], function() {
+// 	gulp.watch(pugFiles, 'pug');
+// 	gulp.watch(scssFiles, 'scss');
+// });
