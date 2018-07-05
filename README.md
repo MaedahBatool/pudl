8c8e9acc1cdf1720836adf407e72969562622954
Maedah Batool <maedahbatool@gmail.com>
2018-07-05 15:52:16 +0500

    📦 NEW: First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Demos
+
+My demos for web development class that I am teaching nowadays.
+
+-   First demo: A custom tip landing page.
---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+/**
+ * Gulpfile.
+ *
+ * @author Maedah Batool (https://github.com/maedahbatool)
+ * @version 1.0.0
+ */
+
+var gulp = require('gulp');
+
+/**
+ * Gulp Task.
+ *
+ */
+gulp.task('hello', function() {
+	console.log('Hello Mao');
+});
