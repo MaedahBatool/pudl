c64cca195b1c65626b58f5c8f84b6c95d55729b9
Maedah Batool <maedahbatool@gmail.com>
2018-07-06 11:46:44 +0500

    📦 NEW: BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,14 @@ const config = {
 	scssSrc: './scss/style.scss',
 	scssDst: './',
 	pugWatchFiles: './views/**/*.pug',
-	scssWatchFiles: './scss/**/*.scss'
+	scssWatchFiles: './scss/**/*.scss',
+	browserSyncWatchFiles: './'
 };
 
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 /**
  * Pug
@@ -45,7 +47,33 @@ gulp.task('scss', function() {
 /**
  * Default
  */
-gulp.task('default', ['pug', 'scss'], function() {
+gulp.task('default', ['pug', 'scss', 'browser-sync'], function() {
 	gulp.watch(config.pugWatchFiles, ['pug']);
 	gulp.watch(config.scssWatchFiles, ['scss']);
+	gulp.watch(config.browserSyncWatchFiles, ['browser-sync']);
+});
+
+/**
+ * BrowserSync Init
+ *
+ */
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
+/**
+ * BrowserSync Watch SCSS Files
+ *
+ */
+gulp.task('serve', ['sass'], function() {
+	browserSync.init({
+		server: './scss'
+	});
+
+	gulp.watch('/scss/*.scss', ['sass']);
+	gulp.watch('scss/*.html').on('change', browserSync.reload);
 });
